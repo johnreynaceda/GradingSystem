afdfe25d515a4d34ecfe32d9917f2c534f9234e7
Jemuel-Emem <jemuelagnes@sksu.edu.ph>
2024-01-18 18:21:58 +0700

    update

--- GradingSystem/Main.Designer.cs
+++ GradingSystem/Main.Designer.cs
@@ -48,8 +48,9 @@
             panel1.Controls.Add(pictureBox1);
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(246, 708);
+            panel1.Size = new Size(281, 944);
             panel1.TabIndex = 0;
             // 
             // studentButton
@@ -60,9 +61,10 @@
             studentButton.ForeColor = Color.White;
             studentButton.Image = (Image)resources.GetObject("studentButton.Image");
             studentButton.ImageAlign = ContentAlignment.MiddleLeft;
-            studentButton.Location = new Point(0, 252);
+            studentButton.Location = new Point(0, 336);
+            studentButton.Margin = new Padding(3, 4, 3, 4);
             studentButton.Name = "studentButton";
-            studentButton.Size = new Size(247, 45);
+            studentButton.Size = new Size(282, 60);
             studentButton.TabIndex = 4;
             studentButton.Text = "STUDENT";
             studentButton.UseVisualStyleBackColor = true;
@@ -73,11 +75,11 @@
             label1.AutoSize = true;
             label1.Font = new Font("Consolas", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.Yellow;
-            label1.Location = new Point(71, 9);
+            label1.Location = new Point(81, 12);
             label1.Name = "label1";
-            label1.Size = new Size(104, 32);
+            label1.Size = new Size(150, 40);
             label1.TabIndex = 3;
-            label1.Text = "UEPLHS";
+            label1.Text = "UEPLHSS";
             // 
             // dashboardButton
             // 
@@ -87,9 +89,10 @@
             dashboardButton.ForeColor = Color.White;
             dashboardButton.Image = (Image)resources.GetObject("dashboardButton.Image");
             dashboardButton.ImageAlign = ContentAlignment.MiddleLeft;
-            dashboardButton.Location = new Point(0, 207);
+            dashboardButton.Location = new Point(0, 276);
+            dashboardButton.Margin = new Padding(3, 4, 3, 4);
             dashboardButton.Name = "dashboardButton";
-            dashboardButton.Size = new Size(247, 45);
+            dashboardButton.Size = new Size(282, 60);
             dashboardButton.TabIndex = 2;
             dashboardButton.Text = "DASHBOARD";
             dashboardButton.UseVisualStyleBackColor = true;
@@ -98,9 +101,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(53, 51);
+            pictureBox1.Location = new Point(61, 68);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(138, 133);
+            pictureBox1.Size = new Size(158, 177);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
@@ -108,18 +112,20 @@
             // pData
             // 
             pData.Dock = DockStyle.Fill;
-            pData.Location = new Point(246, 0);
+            pData.Location = new Point(281, 0);
+            pData.Margin = new Padding(3, 4, 3, 4);
             pData.Name = "pData";
-            pData.Size = new Size(958, 708);
+            pData.Size = new Size(1095, 944);
             pData.TabIndex = 1;
             // 
             // Main
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1204, 708);
+            ClientSize = new Size(1376, 944);
             Controls.Add(pData);
             Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Main";
             Text = "Main";
             panel1.ResumeLayout(false);
