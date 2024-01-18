5958101468f0c37a43b926f68880f04a71f57ea5
Jemuel-Emem <jemuelagnes@sksu.edu.ph>
2024-01-18 21:07:13 +0700

    update role

--- GradingSystem/Login.Designer.cs
+++ GradingSystem/Login.Designer.cs
@@ -37,10 +37,10 @@
             label2 = new Label();
             pictureBox1 = new PictureBox();
             label6 = new Label();
-            textBox1 = new TextBox();
+            txtusername = new TextBox();
             label7 = new Label();
             label8 = new Label();
-            textBox2 = new TextBox();
+            txtpassword = new TextBox();
             signInButton = new Button();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
@@ -55,9 +55,10 @@
             panel1.Controls.Add(label3);
             panel1.Controls.Add(label2);
             panel1.Controls.Add(pictureBox1);
-            panel1.Location = new Point(-1, -2);
+            panel1.Location = new Point(-1, -3);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(230, 463);
+            panel1.Size = new Size(263, 617);
             panel1.TabIndex = 0;
             // 
             // label1
@@ -65,9 +66,9 @@
             label1.AutoSize = true;
             label1.Font = new Font("Verdana", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.FromArgb(64, 64, 64);
-            label1.Location = new Point(59, 224);
+            label1.Location = new Point(67, 299);
             label1.Name = "label1";
-            label1.Size = new Size(118, 29);
+            label1.Size = new Size(147, 36);
             label1.TabIndex = 1;
             label1.Text = "UEPLHS";
             // 
@@ -76,9 +77,9 @@
             label5.AutoSize = true;
             label5.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label5.ForeColor = Color.Silver;
-            label5.Location = new Point(13, 244);
+            label5.Location = new Point(15, 325);
             label5.Name = "label5";
-            label5.Size = new Size(207, 19);
+            label5.Size = new Size(252, 23);
             label5.TabIndex = 5;
             label5.Text = "______________________";
             // 
@@ -86,9 +87,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(12, 314);
+            label4.Location = new Point(14, 419);
             label4.Name = "label4";
-            label4.Size = new Size(207, 19);
+            label4.Size = new Size(252, 23);
             label4.TabIndex = 4;
             label4.Text = "Laboratory High School";
             // 
@@ -96,9 +97,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(56, 295);
+            label3.Location = new Point(64, 393);
             label3.Name = "label3";
-            label3.Size = new Size(108, 19);
+            label3.Size = new Size(131, 23);
             label3.TabIndex = 3;
             label3.Text = "Philippines";
             // 
@@ -106,18 +107,19 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(13, 276);
+            label2.Location = new Point(15, 368);
             label2.Name = "label2";
-            label2.Size = new Size(198, 19);
+            label2.Size = new Size(241, 23);
             label2.TabIndex = 2;
             label2.Text = "University of Eastern";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(48, 76);
+            pictureBox1.Location = new Point(55, 101);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(138, 133);
+            pictureBox1.Size = new Size(158, 177);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -127,28 +129,29 @@
             label6.AutoSize = true;
             label6.Font = new Font("Consolas", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label6.ForeColor = Color.White;
-            label6.Location = new Point(355, 93);
+            label6.Location = new Point(406, 124);
             label6.Name = "label6";
-            label6.Size = new Size(240, 22);
+            label6.Size = new Size(311, 28);
             label6.TabIndex = 1;
             label6.Text = "Sign In to your Account";
             // 
-            // textBox1
+            // txtusername
             // 
-            textBox1.Location = new Point(355, 177);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(240, 30);
-            textBox1.TabIndex = 2;
+            txtusername.Location = new Point(406, 236);
+            txtusername.Margin = new Padding(3, 4, 3, 4);
+            txtusername.Multiline = true;
+            txtusername.Name = "txtusername";
+            txtusername.Size = new Size(274, 39);
+            txtusername.TabIndex = 2;
             // 
             // label7
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label7.ForeColor = Color.White;
-            label7.Location = new Point(355, 155);
+            label7.Location = new Point(406, 207);
             label7.Name = "label7";
-            label7.Size = new Size(81, 19);
+            label7.Size = new Size(98, 23);
             label7.TabIndex = 3;
             label7.Text = "Username";
             // 
@@ -157,27 +160,29 @@
             label8.AutoSize = true;
             label8.Font = new Font("Consolas", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label8.ForeColor = Color.White;
-            label8.Location = new Point(355, 223);
+            label8.Location = new Point(406, 297);
             label8.Name = "label8";
-            label8.Size = new Size(81, 19);
+            label8.Size = new Size(98, 23);
             label8.TabIndex = 5;
             label8.Text = "Password";
             // 
-            // textBox2
+            // txtpassword
             // 
-            textBox2.Location = new Point(355, 245);
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(240, 30);
-            textBox2.TabIndex = 4;
+            txtpassword.Location = new Point(406, 327);
+            txtpassword.Margin = new Padding(3, 4, 3, 4);
+            txtpassword.Multiline = true;
+            txtpassword.Name = "txtpassword";
+            txtpassword.Size = new Size(274, 39);
+            txtpassword.TabIndex = 4;
             // 
             // signInButton
             // 
             signInButton.BackColor = Color.Yellow;
             signInButton.Font = new Font("Consolas", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            signInButton.Location = new Point(355, 305);
+            signInButton.Location = new Point(406, 407);
+            signInButton.Margin = new Padding(3, 4, 3, 4);
             signInButton.Name = "signInButton";
-            signInButton.Size = new Size(241, 37);
+            signInButton.Size = new Size(275, 49);
             signInButton.TabIndex = 6;
             signInButton.Text = "Sign In";
             signInButton.UseVisualStyleBackColor = false;
@@ -185,21 +190,23 @@
             // 
             // Login
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Blue;
-            ClientSize = new Size(714, 460);
+            ClientSize = new Size(816, 613);
             Controls.Add(signInButton);
             Controls.Add(label8);
-            Controls.Add(textBox2);
+            Controls.Add(txtpassword);
             Controls.Add(label7);
-            Controls.Add(textBox1);
+            Controls.Add(txtusername);
             Controls.Add(label6);
             Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Login";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            Load += Login_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -217,10 +224,10 @@
         private Label label2;
         private Label label1;
         private Label label6;
-        private TextBox textBox1;
+        private TextBox txtusername;
         private Label label7;
         private Label label8;
-        private TextBox textBox2;
+        private TextBox txtpassword;
         private Button signInButton;
     }
 }
--- GradingSystem/Login.cs
+++ GradingSystem/Login.cs
@@ -1,7 +1,13 @@
+using System;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Security.Principal;
+
 namespace GradingSystem
 {
     public partial class Login : Form
     {
+        private const string ConnectionString = "Server=localhost;Database=ueplhs;Uid=root";
         public Login()
         {
             InitializeComponent();
@@ -9,9 +15,69 @@ namespace GradingSystem
 
         private void signInButton_Click(object sender, EventArgs e)
         {
-            Main frmMain = new Main();
-            frmMain.Show();
+            string username = txtusername.Text;
+            string password = txtpassword.Text;
+
+            string query = "SELECT role FROM accounts WHERE username = @Username AND password = @Password";
+
+            using (MySqlConnection connection = new MySqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    cmd.Parameters.AddWithValue("@Password", password);
+
+                    object result = cmd.ExecuteScalar();
+
+                    if (result != null)
+                    {
+                        string role = result.ToString();
+                        OpenMainFormBasedOnRole(role);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid username or password");
+                    }
+                }
+            }
+        }
+
+        private void OpenMainFormBasedOnRole(string role)
+        {
+           
+            switch (role)
+            {
+                case "Adviser":
+                    MessageBox.Show("Adiser");
+                    break;
+                case "Subject Teacher":
+                    MessageBox.Show("Subject Teacher");
+                    break;
+                case "Principal":
+                    MessageBox.Show("Principal");
+                    break;
+                case "Secretary":
+                    MessageBox.Show("Secretary");
+                    break;
+                case "Academic Chair":
+                    MessageBox.Show("Academic Chair");
+                    break;
+                case "Admin":
+                    MessageBox.Show("Admin");
+                    break;
+                default:
+                    MessageBox.Show("Unknown role");
+                    break;
+            }
+
             this.Hide();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
